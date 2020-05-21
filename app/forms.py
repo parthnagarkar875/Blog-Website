e785cf09a68b971660a1e8c6fd02b7cb13e7115b
VirtualGoat <nagarkarparth875@gmail.com>
2020-05-21 22:37:22 +0530

    Update user details,DP, DP resize added

--- app/forms.py
+++ app/forms.py
@@ -7,9 +7,12 @@ Created on Wed Apr 29 20:09:35 2020
 
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
+from flask_login import current_user
+
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username', 
@@ -43,4 +46,25 @@ class LoginForm(FlaskForm):
     remember=BooleanField('Remember Me')    
     submit = SubmitField('Login')
     
+
+class UpdateAccountForm(FlaskForm):
+    username=StringField('Username', 
+                         validators= [DataRequired(), Length(min=2,max=20)])
+    email=StringField('Email', 
+                         validators= [DataRequired(), Email()])
+            
+    submit = SubmitField('Update')    
+    
+    picture=FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
+    
+    def validate_username(self,username):
+        if username.data!=current_user.username:
+            user= User.query.filter_by(username=username.data).first()        
+            if user:
+                raise ValidationError('That username is taken!')
         
+    def validate_email(self,email):
+        if email.data!=current_user.email:
+            user= User.query.filter_by(email=email.data).first()        
+            if user:
+                raise ValidationError('That email is taken!')
--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,15 @@ Created on Sat May  2 21:09:53 2020
 @author: Parth
 """
 
+import secrets
+from PIL import Image
 from flask import Flask,redirect,request,render_template,flash,url_for     #url_for is used for routing through links. We have used it while linking the CSS file. 
 from app.models import User, Post
 from datetime import datetime
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm,UpdateAccountForm
 from app import app,db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
+import os
 
 # =============================================================================
 # WSGI= Web Server Gateway Interface. 
@@ -75,7 +78,19 @@ def logout():
     logout_user()
     return redirect(url_for('home'))    
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _,f_ext=os.path.splitext(form_picture.filename)
+    picture_fn=random_hex + f_ext
+    picture_path=os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size=(125,125)
+    i=Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+    
+@app.route('/account',methods=['GET','POST'])
 @login_required              #Ensures that the user is logged in before accessing the page. 
 # =============================================================================
 # In the __init__.py file, I have declared a variable login_manager.login_view='login' 
@@ -83,7 +98,21 @@ def logout():
 # that requires him/her to login in the first place.
 # =============================================================================
 def account():
-    return render_template('account.html',title='Account')
+    form=UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash('Your account has been updated!','Success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data =current_user.username
+        form.email.data =current_user.email
+    image_file=url_for('static', filename='profile_pics/'+ current_user.image_file)
+    return render_template('account.html',title='Account', image_file=image_file, form=form)
 
 
 
