9ee8b1d5538496f5bdc9eea1b2466d1891e13bca
VirtualGoat <nagarkarparth875@gmail.com>
2020-05-22 23:57:25 +0530

    Added posts functionality

--- app/routes.py
+++ app/routes.py
@@ -114,9 +114,10 @@ def new_post():
     return render_template('create_post.html',title='New Post', form = form)
             
     
-
-
-
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post.Post.query.get_or_404(post_id)
+    return render_template('post.html',title=post.title, post=post)
 
 
 
