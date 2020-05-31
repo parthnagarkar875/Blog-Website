8d2ebcbd35e0b0edaca6903ec0723dfd2f7622be
VirtualGoat <nagarkarparth875@gmail.com>
2020-05-31 23:04:23 +0530

    Deploying to heroku

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,31 @@
-from run import app
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 02:06:46 2020
 
-if __name__=="__main__":
-    app.run()
+@author: Parth
+"""
+from app import create_app
+
+app=create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+'''
+Commands used to create database, insert values and print values:
+--> from app import db
+--> db.create_all()         #This creates the database structure. 
+--> from app import User, Post
+--> user_1 = User(username='Parth', email='nagarkarparth@gmail.com', password='goat')
+--> db.session.add(user_1)
+--> db.session.commit()
+--> User.query.first()      #Gives the first row stored in the database. 
+--> user= user.query.filter_by(username='Parth').first()
+--> post_1 = Post(title= 'Blog 1', content='First Post Content', user_id=user.id) 
+--> db.session.add(post_1)
+--> db.session.commit()
+--> user.posts
+--> post.author
+'''
+
